f06bdd097be91609e8f21d301d4e0dc5a78b0647
Yongwei Yuan <slark@umich.edu>
2020-03-23 19:10:55 -0400

    simple implementation to determine if a set of constraints is inconsistent

---
+++ inconsistent-constraints.ml
@@ -0,0 +1,66 @@
+module Constraint = struct
+  type t =
+    | Truth | Falsity
+    | And of t * t | Or of t * t
+    | Inl of t | Inr of t
+    | Pair of t * t
+end
+
+let partition2 (pred1 : 'a -> bool) (pred2 : 'a -> bool) (cs : 'a list) : ('a list * 'a list) option =
+  let f (acc : ('a list * 'a list) option) (c : 'a) =
+    match acc with
+    | None -> None
+    | Some (trues, falses) ->
+        if pred2 c then None
+        else if pred1 c then
+          Some (c::trues, falses)
+        else 
+          Some (trues, c::falses)
+  in
+  List.fold_left f (Some ([], [])) cs
+
+let rec is_inconsistent (cs : Constraint.t list) : bool =
+  match cs with
+  | [] -> false
+  | c::cs' -> (
+    match c with
+    | Truth -> is_inconsistent cs'
+    | Falsity -> false
+    | And (c1, c2) -> is_inconsistent (c1::c2::cs')
+    | Or (c1, c2) -> is_inconsistent (c1::cs') && is_inconsistent (c2::cs')
+    | Inl _ -> (
+      match
+        partition2
+        (function Constraint.Inl c -> true | _ -> false)
+        (function Constraint.Inr c -> true | _ -> false)
+        cs
+      with
+      | None -> false
+      | Some (inls, []) -> is_inconsistent (
+        List.map (function Constraint.Inl c -> c | _ -> assert false) inls
+      )
+      | Some (inls, other) -> is_inconsistent (other @ inls)
+    )
+    | Inr c' -> (
+      match
+        partition2
+        (function Constraint.Inr c -> true | _ -> false)
+        (function Constraint.Inl c -> true | _ -> false)
+        cs
+      with
+      | None -> false
+      | Some (inrs, []) -> is_inconsistent (
+        List.map (function Constraint.Inr c -> c | _ -> assert false) inrs
+      )
+      | Some (inrs, other) -> is_inconsistent (other @ inrs)
+    )
+    | Pair (c1, c2) -> is_inconsistent (c1::cs') || is_inconsistent (c2::cs')
+  )
+
+let is_inconsistent_tests : (Constraint.t list * bool) list = [
+  ( [Inl Truth; Inr Truth; Truth], true ) ;
+  ( [Truth; Falsity], false ) ;
+  ( [And (Inl Truth, Truth); Inr Truth], false ) ;
+  ( [Or (Falsity, Truth)], false ) ;
+  ( [Pair (Inr Falsity, Truth)], true) ;
+]
